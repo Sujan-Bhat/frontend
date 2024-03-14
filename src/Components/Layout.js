0de7732c183619a691d4c28ac160810d7f7587a9
Sujan-Bhat <sujathahegde94@gmail.com>
2024-03-14 19:58:23 +0530

    Order now

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -3,6 +3,7 @@ import { useGetMeQuery } from "../redux/api/userApi";
 import { useSelector } from "react-redux";
 import { useLazyLogoutQuery } from "../redux/api/authApi";
 import "../Components/styles/homepage.css";
+import logo from "../Components/assets/pizza.png";
 
 const Layout = () => {
   const navigate = useNavigate();
@@ -19,43 +20,52 @@ const Layout = () => {
 
   return (
     <div className="body-of-layout">
-      <header className="header">
-        <img src="" alt="logo"></img>
-        <div className="navi">
-          <nav className="navigation-bar">
-            <ul className="listu">
-              <li className="list">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="list">
-                <Link to="">Menu</Link>
-              </li>
-              {user ? (
-                <>
-                  <Link>{user?.name}</Link>
-                  <br />
-                  <Link to="/" onClick={logoutHandler}>
-                    Logout
-                  </Link>
-                </>
-              ) : (
-                !isLoading && (
+      <div className="upperh">
+        <header className="header">
+          <img src={logo} alt="logo" />
+          <div className="navi">
+            <nav className="navigation-bar">
+              <ul className="listu">
+                <li className="list">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list">
+                  <Link to="">Menu</Link>
+                </li>
+                {user ? (
                   <>
-                    <li className="list">
-                      <Link to="/login">Login</Link>
-                    </li>
-                    <li className="list">
-                      <Link to="/register">Register</Link>
-                    </li>
+                    <Link>{user?.name}</Link>
+                    <br />
+                    <Link to="/" onClick={logoutHandler}>
+                      Logout
+                    </Link>
                   </>
-                )
-              )}
-            </ul>
-          </nav>
+                ) : (
+                  !isLoading && (
+                    <>
+                      <li className="list">
+                        <Link to="/login">Login</Link>
+                      </li>
+                      <li className="list">
+                        <Link to="/">Cart</Link>
+                      </li>
+                    </>
+                  )
+                )}
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <Outlet />
+      </div>
+      <main className="main">
+        <div className="order">
+          <button className="ordernow">Order now</button>
         </div>
-      </header>
-      <Outlet />
-      <main></main>
+      </main>
+      <div className="lowerh">
+        <footer></footer>
+      </div>
     </div>
   );
 };
